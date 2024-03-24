2902cc0ba5e3cd6c50565139165f603500f6648c
Huỳnh Nhật Quang <70760687+Quang292001@users.noreply.github.com>
2024-03-24 20:43:38 +0700

    Add files via upload

---
+++ ServiceDiscovery/src/test/java/com/example/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
